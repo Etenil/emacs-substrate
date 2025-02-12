a7406fb2a375bdc358361349c30850f8e523b46f
Gene Pasquet <dev@etenil.net>
2025-02-12 07:01:38 +0000

    Start adding a installer

---
+++ installer/early-init.el
@@ -0,0 +1,4 @@
+;; ~/.emacs.d/early-init.el
+(add-to-list 'load-path "INSTALL_PATH")
+
+(require 'early-substrate)
---
+++ installer/init.el
@@ -0,0 +1,8 @@
+;; ~/.emacs.d/init.el
+(require 'substrate)
+
+;; Set custom variables here
+
+(substrate-init)
+
+;; The rest of your config below
